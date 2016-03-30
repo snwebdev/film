2f7758e48193295b004c43a26c780a695cf46250
Stewart Nairn <stewartnairn@live.com>
2016-03-30 21:35:08 +0100

    works

--- loop.js
+++ loop.js
@@ -1,65 +1,78 @@
 
-
 $.getJSON("times.php", function (times) {
-//alert(times);
+
     var wholeListHTML = "";
+
     for (var i = 0; i < times.length; i++) {
-        wholeListHTML += "<li  class='timeFilmCinema list-group-item'  data-id='" + i + "'>" +
-                "<span class='time'>" +
+        wholeListHTML += "<li  class='selectable bothItems list-group-item'  data-id='" + i + "'>" +
+                "<div class='time'>" +
                 times[i][0] +
-                "</span>" +
+                "</div>" +
+                "<div class='secondItem'>" +
                 "<span class='filmName'>" +
                 times[i][1] +
                 "</span>" +
                 ", " +
                 times[i][2] +
+                "</div>" +
                 "</li>";
     }
 
+    resizeOutput();
     $("#output").html(wholeListHTML);
 
-    $(document).on("click", "li", function(event){
-      
-   var bigListIndex =  $(this).data("id");
-    
+    $(document).on("click", ".selectable", function () {
+
+        var bigListIndex = $(this).data("id");
+
         var film = times[bigListIndex][1];
         var cinema = times[bigListIndex][2];
-       
-       $("#moreCinemas").text("Cinemas showing " + film);
-        $("#moreFilms").text("Films at " + cinema);
-        
-    $("#myModal").modal("show");
-    
-    $("#myModal li").on("click", function (event) {
 
-        switch (event.target.id) {
-          
-            case "moreFilms":
-                
-                filmsForCinema(bigListIndex, $(event.target).text());
-                break;
-            case "moreCinemas":
-                cinemasForFilm(bigListIndex, $(event.target).text());
-                break;
-            case "close":
+        $("#moreCinemas").text("Cinemas showing " + film);
+        $("#moreFilms").text("Films in " + cinema);
 
-             $("#myModal").modal("hide");
-                break;
-        }
+        $("#myModal").modal("show");
+
+        $("#myModal li").on("click", function (event) {
+
+            switch (event.target.id) {
+
+                case "moreFilms":
+
+                    filmsForCinema(bigListIndex, $(event.target).text());
+                    break;
+
+                case "moreCinemas":
+
+                    cinemasForFilm(bigListIndex, $(event.target).text());
+                    break;
+
+                case "close":
+
+                    $("#myModal").modal("hide");
+                    break;
+            }
+        });
+
+
+
+        $("#back").on("click", function () {
+            $("#back").css("visibility", "hidden");
+            $("#heading").html("Films around <span id='city'>Glasgow</span>");
+            $("#output").html(wholeListHTML);
+
+        });
+
     });
-    
-    $("#back").on("click", function(){
-        $("#back").css("visibility", "hidden");
-        $("#heading").html("Films in <span id='city'>Glasgow</span>");
-          $("#output").html(wholeListHTML);
-    });
-   
-    });
 
-     function cinemasForFilm(index) {
-         $("#back").css("visibility", "visible");
 
-        var html = "";
+
+
+
+    function cinemasForFilm(index, heading) {
+        $("#back").css("visibility", "visible");
+
+     
         var cinemasForFilm = [];
 
         for (var i = 0; i < times.length; i++) {
@@ -67,24 +80,28 @@ $.getJSON("times.php", function (times) {
                 cinemasForFilm.push([times[i][0], times[i][2]]);
             }
         }
- 
+
+
+        var html = "";  
         for (var i = 0; i < cinemasForFilm.length; i++) {
-            html += "<li data-toggle='modal' data-target='#myModal' data-backdrop='static' class='timeFilm list-group-item' data-id='" + i + "'>" +
-                    "<span class='time'>" +
+            html += "<li class='bothItems list-group-item' data-id='" + i + "'>" +
+                    "<div class='time'>" +
                     cinemasForFilm[i][0] +
-                    "</span>" +
-                   cinemasForFilm[i][1] +
-                    "</li>";
+                    "</div><div class='secondItem'>" +
+                    cinemasForFilm[i][1] +
+                    "</div></li>";
         }
-        // alert("passed secon loop");
+     
+
         $("#myModal").modal("hide");
-        $("#heading").html($(event.target).text());
+        $("#heading").html(heading);
+        resizeOutput();
         $("#output").html(html);
 
     }
 
 
-    
+
 
     function filmsForCinema(index, heading) {
         $("#back").css("visibility", "visible");
@@ -96,28 +113,27 @@ $.getJSON("times.php", function (times) {
                 filmsForCinema.push([times[i][0], times[i][1]]);
             }
         }
-       
+
         for (var i = 0; i < filmsForCinema.length; i++) {
-            html += "<li data-toggle='modal' data-target='#myModal' data-backdrop='static' class='timeFilm list-group-item' data-id='" + i + "'>" +
-                    "<span class='time'>" +
+            html += "<li class='bothItems list-group-item' data-id='" + i + "'>" +
+                    "<div class='time'>" +
                     filmsForCinema[i][0] +
-                    "</span>" +
+                    "</div><div class='secondItem'>" +
                     filmsForCinema[i][1] +
-                    "</li>";
+                    "</div></li>";
         }
-        // alert("passed secon loop");
+
         $("#myModal").modal("hide");
         $("#heading").html(heading);
+        resizeOutput();
         $("#output").html(html);
 
     }
-    
-    
 
 
 });
 
- 
+
 
 
 
--- test.php
+++
@@ -1,7 +0,0 @@
-
-      <?php
-      $myArray = array ("mince","potatoes","gravy");
-       echo json_encode($myArray);
-?>
-  
-  
